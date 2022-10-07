4d330e850c01b41b1b212ff8691de307abb5ce27
elena.vanengelen <elena.vanengelen@edsn.nl>
2022-10-07 21:02:46 +0200

    remove permission-boundary-aspect.ts

--- infrastructure/bin/infrustructure.ts
+++ infrastructure/bin/infrustructure.ts
@@ -4,7 +4,6 @@ import {App, Stack, Tags} from 'aws-cdk-lib';
 import {InfrastructureX86Stack} from '../lib/infrastructure-x86-stack';
 import {InfrastructureARM64Stack} from "../lib/infrastructure-arm64-stack";
 import {InfrastructureTableStack} from "../lib/infrastructure-table-stack";
-//import PermissionsBoundaryAspect from "../lib/permission-boundary-aspect";
 
 const app = new App();
 
@@ -33,9 +32,6 @@ const stackARM64 = new InfrastructureARM64Stack(app, stackNameARM64, {
   description: 'GraalVM ARM64 example',
 });
 
-// const permissionBoundary = new PermissionsBoundaryAspect(
-//   `arn:aws:iam::${Aws.ACCOUNT_ID}:policy/xxx-base-permissions-boundary`
-// );
 for (const node of app.node.children) {
   if (node instanceof Stack) {
     Tags.of(node).add('Application ID', node.stackName);
